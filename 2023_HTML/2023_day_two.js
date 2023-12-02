c65d98cdb2b49b2aded5c760f19f05bb552bd718
Nathan313 <ndavey97@gmail.com>
2023-12-01 22:15:25 -0800

    Add files via upload

--- 2023_HTML/2023_day_two.js
+++ 2023_HTML/2023_day_two.js
@@ -13,20 +13,83 @@ function readTextFile(file){
     return text;
     
 }
+
 function partOne(){
     let fileData = readTextFile("../2023_inputFiles/daytwoinput.txt");
     newArray = fileData.split("\n");
-    console.log("test");
+    var maxCubes = {
+        'red': 12,
+        'green': 13,
+        'blue': 14 
+    };
+    var runningTotal = 0;
+    for(var i = 0; i < newArray.length; i++){
+        var currentValid = true;
+        //replace text Game followed by any amount of digits, then a colon and a space.
+        var currentLine = newArray[i].trim().split(':');
+        var currentLine = currentLine[1].split(';');
+        for(var z = 0; z < currentLine.length; z++){
+            var currentGame = currentLine[z];
+            currentGame = currentGame.split(',');
+            if(currentValid){
+                for(var y = 0; y < currentGame.length; y++){
+                    var currentCube = currentGame[y].trim().split(' ');
+                    if(maxCubes[currentCube[1]] < currentCube[0]){
+                        currentValid = false;
+                        break;
+                    }
+                }
+            }
+            else{
+                break;
+            }
+        }
+        if(currentValid){
+            runningTotal += parseInt(i+1);
+        }
+    }
+    console.log(runningTotal);
 }
+
+
+
+
+
+
+
 
 
 function partTwo(){
     let fileData = readTextFile("../2023_inputFiles/daytwoinput.txt");
     newArray = fileData.split("\n");
-
+    var runningTotal = 0;
+    for(var i = 0; i < newArray.length; i++){
+        var minRequired = {
+            'red': 0,
+            'green': 0,
+            'blue': 0 
+        }
+        var powerSet = 0;
+        var currentLine = newArray[i].trim().split(':');
+        var currentLine = currentLine[1].split(';');
+        for(var z = 0; z < currentLine.length; z++){
+            var currentGame = currentLine[z];
+            currentGame = currentGame.split(',');
+            for(var y = 0; y < currentGame.length; y++){
+                var currentCube = currentGame[y].trim().split(' ');
+                if(parseInt(minRequired[currentCube[1]]) < parseInt(currentCube[0])){
+                    minRequired[currentCube[1]] = currentCube[0];
+                }
+            }
+        }
+        powerSet = parseInt(minRequired['red']*minRequired['green']*minRequired['blue']);
+        runningTotal += powerSet;
+    }
+    console.log(runningTotal); 
 }
+
 
 window.onload = function(){
 partOne();
-//partTwo();
+partTwo();
 }
